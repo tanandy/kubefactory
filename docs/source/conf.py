e1cd98ff551d1e49037650db541f512d6dc29eb4
Aurélien Maury <cto@wescale.fr>
2021-02-01 00:45:48 +0100

    doc

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2021, WeScale'
 author = 'WeScale'
 
 # The full version, including alpha/beta/rc tags
-release = 'v0.0.6'
+release = 'v0.0.X'
 
 # -- General configuration ---------------------------------------------------
 
